96b8d817bb0411fc836bd15921b7a892f54216a5
= <=>
2021-05-28 22:11:12 +0700

    AssertJ

---
+++ 02-AssertJ/Demo/src/main/java/vn/techmaster/Dog.java
@@ -0,0 +1,11 @@
+package vn.techmaster;
+
+public class Dog {
+    private String name;
+    private double weight;
+
+    public Dog(String name, double weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+}
